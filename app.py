ce48dc375c988bd16772514b0d5236c311e94147
AnilopaM <anilopa.m@mail.ru>
2023-12-03 21:17:20 +0300

    Лабораторная работа №3

---
+++ app.py
@@ -0,0 +1,70 @@
+import PySimpleGUI as sg
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+def load_tokenizer_and_model(model_name_or_path):
+    return GPT2Tokenizer.from_pretrained(model_name_or_path), GPT2LMHeadModel.from_pretrained(model_name_or_path)
+
+def generate(
+    model, tok, text,
+    do_sample=True, max_length=50, repetition_penalty=5.0,
+    top_k=5, top_p=0.95, temperature=1,
+    num_beams=None,
+    no_repeat_ngram_size=3
+):
+    input_ids = tok.encode(text, return_tensors="pt")
+    out = model.generate(
+        input_ids,
+        max_length=max_length,
+        repetition_penalty=repetition_penalty,
+        do_sample=do_sample,
+        top_k=top_k, top_p=top_p, temperature=temperature,
+        num_beams=num_beams, no_repeat_ngram_size=no_repeat_ngram_size
+    )
+    return list(map(tok.decode, out))
+
+def generate_model_response(text, num_beams=5, max_length=240):
+    tokenizer, model = load_tokenizer_and_model("sberbank-ai/rugpt3small_based_on_gpt2")
+    return generate(model, tokenizer, text, max_length=max_length, num_beams=num_beams)[0]
+
+def apply_filter(texts, substring):
+    for i in range(len(texts)):
+        for symbol in ["\n", "&nbsp;", "&laquo;"]:
+            texts[i] = texts[i].replace(symbol, "")
+        texts[i] = texts[i][:texts[i].rfind(".") + 1]
+    res = texts[0].replace(substring, "")    
+    return res
+
+def get_annotation(text):
+    text = f"Курсовая работа по теме: {text.lower()}. Аннотация: "
+    return apply_filter([generate_model_response(text, max_length=130)], text)
+
+def get_introduction(text):
+    ss = f"Курсовая работа по теме: {text.lower()}. "
+    text = ss + "Актуальность работы подтверждается "
+    return apply_filter([generate_model_response(text, max_length=300)], ss)
+
+layout = [
+    [sg.Text('Тема КНИР: ', text_color='#000000', background_color='#FFFFFF'), sg.InputText(size=(89, 1), key='input')],
+    [sg.Text('Аннотация: ', text_color='#000000', background_color='#FFFFFF')],
+    [sg.Output(size=(100, 8), key='-OUTPUT1-')],
+    [sg.Text('Актуальность работы: ', text_color='#000000', background_color='#FFFFFF')],
+    [sg.Output(size=(100, 15), key='-OUTPUT2-')],
+    [sg.Submit('Генерировать'), sg.Submit('Закрыть')]
+]
+
+window = sg.Window('Генерация русскоязычного текста', layout, background_color='#FFFFFF')
+
+while True:  
+    event, values = window.read()
+    if event in (None, 'Exit', 'Cancel', 'Закрыть'):
+        break
+    if event == 'Генерировать':
+        window['-OUTPUT1-'].update('')
+        window['-OUTPUT2-'].update('')
+        try:
+            window['-OUTPUT1-'].update(get_annotation(values['input']))
+            window['-OUTPUT2-'].update(get_introduction(values['input']))
+        except Exception as e:
+            print(f"Критическая ошибка: {e}")
+
+window.close()
